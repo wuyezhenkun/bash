c46a1614632763ac606ccdfcd0f9957f6e020a4d
WeiliangBMBL <liu.3540@osu.edu>
2019-04-19 18:04:58 -0400

    Update general_pipeline.bash

--- general_pipeline.bash
+++ general_pipeline.bash
@@ -13,23 +13,23 @@ set –x
 
 
 
-## general_pipeline comand 1  (mapping)
+## general_pipeline command 1  (mapping)
 /home/weiliang/BMBL/metaqubic-master/runmeta --mapping -s /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/data/samples/ -ref /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/data/ref/IGC_ref.fa -o1 /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/align_out/ -o2 /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/cat/
 
 
-## general_pipeline comand 2  (CatToMat)
+## general_pipeline command 2  (CatToMat)
 /home/weiliang/BMBL/metaqubic-master/runmeta --CatToMat -i /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/cat/ -m /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/data/735_hGEM/ -o /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/hGEM/
 
 
-## general_pipeline comand 3  (filtering)
+## general_pipeline command 3  (filtering)
 /home/weiliang/BMBL/metaqubic-master/runmeta --filtering -i /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/hGEM/ -minTPM 1 -NominTPM 2 -o /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/hGEM_filt/
 
 
-## general_pipeline comand 4  (biclustering)
+## general_pipeline command 4  (biclustering)
 /home/weiliang/BMBL/metaqubic-master/runmeta --biclustering -i /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/hGEM_filt/ -o /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/bic/ --QUBIC -c 1 -f 0.8 -o 100 
 
 
-## general_pipeline comand 5  (enrichment)
+## general_pipeline command 5  (enrichment)
 /home/weiliang/BMBL/metaqubic-master/runmeta --enrichment -i /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/bic -g /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/data/annotation/KEGG_ann.txt -o /pylon5/cc5fpcp/weiliang/BMBL/metaqubic/general_pipeline/results/enrich_out/
 
 
